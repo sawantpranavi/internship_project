17f8626d0310350341f8da54ac9f1664c9a9b012
sawantpranavi <sawantpranavi03>
2024-08-13 00:21:12 +0530

    minor changes

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/index", (req, res) => {
+    res.render("index");
+});
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 })
